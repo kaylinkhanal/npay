eac6e65b5feb4f98ec0cd7eee30061f73a154468
Kaylin Khanal <kyalinnop@gmail.com>
2024-06-03 09:22:24 +0545

    added backend register code

--- server/index.js
+++ server/index.js
@@ -24,20 +24,36 @@ const userSchema = new Schema({
     default: 'user'
   },
 });
-
-
 const User = mongoose.model('User', userSchema);
 const port = process.env.PORT || 8000
 
+app.post('/register', async (req, res) => {
+  const phoneExist  = await User.exists({phoneNumber: req.body.phoneNumber})
+  const emailExist  = await User.exists({email: req.body.email})
 
-app.post('/register', (req, res) => {
-  User.create(req.body)
-  res.send('ok')
+  if(phoneExist ){
+   return res.json({msg: "Phone Number is taken!"})
+  }else if(emailExist){
+    return res.json({msg: "Email is taken!"})
+  }
+  await User.create(req.body)
+  return res.json({msg: "User registered"})
 })
 
-app.get('/users', async (req, res) => {
+app.post('/login',async(req,res)=>{
+  //STEP 1:
+  //check if phone number exist
+  //
+  // NO: res.json({msg: "User not registered"})
+  // YES: 
+    // check if password matches
+      // NO: res.json({msg: "Incorrect password"})
+      //YES: token
+})
+
+app.get('/users',async(req,res)=>{
   const data = await User.find()
-  res.send(data)
+  res.json(data)
 })
 
 app.listen(port, () => {
